4ee30d035674522cf5790074fa60c281387c065f
Alexander Vokhmin <aavkhm@gmail.com>
2021-09-12 15:22:59 +0700

    Реализовано чтение таблицы

--- src/test/java/PaymentCheck.java
+++ src/test/java/PaymentCheck.java
@@ -12,7 +12,7 @@ public class PaymentCheck extends WebDriverSettings{
 
     @Test
     public void testUntitledTestCase() throws Exception {
-        driver.get("https://sandbox.cardpay.com/MI/cardpayment2.html?orderXml=PE9SREVSIFdBTExFVF9JRD0nODI5OScgT1JERVJfTlVNQkVSPSc0NTgyMTEnIEFNT1VOVD0nMjkxLjg2JyBDVVJSRU5DWT0nRVVSJyAgRU1BSUw9J2N1c3RvbWVyQGV4YW1wbGUuY29tJz4KPEFERFJFU1MgQ09VTlRSWT0nVVNBJyBTVEFURT0nTlknIFpJUD0nMTAwMDEnIENJVFk9J05ZJyBTVFJFRVQ9JzY3NyBTVFJFRVQnIFBIT05FPSc4NzY5OTA5MCcgVFlQRT0nQklMTElORycvPgo8L09SREVSPg==&sha512=998150a2b27484b776a1628bfe7505a9cb430f276dfa35b14315c1c8f03381a90490f6608f0dcff789273e05926cd782e1bb941418a9673f43c47595aa7b8b0d");
+
         driver.findElement(By.id("input-card-number")).click();
         driver.findElement(By.id("input-card-number")).sendKeys(cardInfo[0][0]);
         driver.findElement(By.id("input-card-holder")).sendKeys("TEST TEST");
@@ -28,4 +28,23 @@ public class PaymentCheck extends WebDriverSettings{
         driver.findElement(By.id("payment-status")).click();
         assertEquals("Success", driver.findElement(By.xpath("//div[@id='payment-status-title']/span")).getText());
     }
+    @Test
+    public void testUntitledTestCasTwo() throws Exception {
+
+        driver.findElement(By.id("input-card-number")).click();
+        driver.findElement(By.id("input-card-number")).sendKeys(cardInfo[0][0]);
+        driver.findElement(By.id("input-card-holder")).sendKeys("TEST TEST");
+        driver.findElement(By.id("card-expires-month")).click();
+        new Select(driver.findElement(By.id("card-expires-month"))).selectByVisibleText("12");
+        driver.findElement(By.id("payment-data-card-fields")).click();
+        driver.findElement(By.id("card-expires-year")).click();
+        new Select(driver.findElement(By.id("card-expires-year"))).selectByVisibleText("2035");
+        driver.findElement(By.id("input-card-cvc")).click();
+        driver.findElement(By.id("input-card-cvc")).sendKeys("777");
+        driver.findElement(By.id("action-submit")).click();
+        driver.findElement(By.id("success")).click();
+        driver.findElement(By.id("payment-status")).click();
+        assertEquals("Success", driver.findElement(By.xpath("//div[@id='payment-status-title']/span")).getText());
+    }
+
 }
--- src/test/java/WebDriverSettings.java
+++ src/test/java/WebDriverSettings.java
@@ -1,9 +1,10 @@
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
@@ -11,38 +12,34 @@ import static org.junit.Assert.assertEquals;
 public class WebDriverSettings {
 
     public ChromeDriver driver, driver1;
-    public String [][] cardInfo = new String[10][2];
-
+    public String [][] cardInfo;
+    public String OrderNumber, Value;
     @Before
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver", "src\\test\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+       driver.get("https://sandbox.cardpay.com/MI/cardpayment2.html?orderXml=PE9SREVSIFdBTExFVF9JRD0nODI5OScgT1JERVJfTlVNQkVSPSc0NTgyMTEnIEFNT1VOVD0nMjkxLjg2JyBDVVJSRU5DWT0nRVVSJyAgRU1BSUw9J2N1c3RvbWVyQGV4YW1wbGUuY29tJz4KPEFERFJFU1MgQ09VTlRSWT0nVVNBJyBTVEFURT0nTlknIFpJUD0nMTAwMDEnIENJVFk9J05ZJyBTVFJFRVQ9JzY3NyBTVFJFRVQnIFBIT05FPSc4NzY5OTA5MCcgVFlQRT0nQklMTElORycvPgo8L09SREVSPg==&sha512=998150a2b27484b776a1628bfe7505a9cb430f276dfa35b14315c1c8f03381a90490f6608f0dcff789273e05926cd782e1bb941418a9673f43c47595aa7b8b0d");
+        OrderNumber = driver.findElement(By.id("order-number")).getText();
+        System.out.println(OrderNumber);
+
     }
     @Before
     public void getCardsInfo() {
         driver1 = new ChromeDriver();
         driver1.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver1.get("https://integration.unlimint.com/v1/api/#environments");
-/*
-        ArrayList<String> CardNumber = new ArrayList<String>();
 
-        CardNumber.add(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Transaction result'])[1]/following::td[1]")).getText());
-        CardNumber.add(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Payment has CONFIRMED status'])[1]/following::td[1]")).getText());
-        CardNumber.add(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Payment has DECLINED status'])[1]/following::td[1]")).getText());
-        CardNumber.add(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Payment has AUTHORIZED status'])[1]/following::td[1]")).getText());
-        CardNumber.add(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Payment has CONFIRMED status'])[2]/following::td[1]")).getText());
-        CardNumber.add(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Payment has CONFIRMED status'])[3]/following::td[1]")).getText());
-        CardNumber.add(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Payment has CONFIRMED status'])[4]/following::td[1]")).getText());
-        CardNumber.add(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Payment has CONFIRMED status'])[5]/following::td[1]")).getText());
-        CardNumber.add(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Payment has CONFIRMED status'])[6]/following::td[1]")).getText());
-        CardNumber.add(driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Payment has DECLINED status'])[2]/following::td[1]")).getText());
-*/
+        WebElement baseTableTitle = driver1.findElement(By.xpath("/html/body/div[2]/div[2]/table[1]"));
+        WebElement baseTableBody = driver1.findElement(By.xpath("/html/body/div[2]/div[2]/table[1]/tbody"));
+        List<WebElement> tableRows = baseTableBody.findElements(By.tagName("tr"));
+        List<WebElement> tableColumns = baseTableTitle.findElements(By.tagName("th"));
 
-        //НОМЕР КАРТЫ
-        cardInfo[0][0] = driver1.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Transaction result'])[1]/following::td[1]")).getText();
-        //СТАТУС ОПЕРАЦИИ
-        cardInfo[0][1] = driver1.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Transaction result'])[1]/following::td[3]")).getText();
+        cardInfo = new String[tableRows.size()][tableColumns.size()];
+        for (int i = 0; i < cardInfo.length; i++)
+            for (int j = 0; j < cardInfo[0].length; j++)
+                cardInfo[i][j] = driver1.findElement(By.xpath("/html/body/div[2]/div[2]/table[1]/tbody/tr[" + (i+1)+"]/td[" + (j+1) + "]")).getText();
+
         driver1.quit();
     }
 
